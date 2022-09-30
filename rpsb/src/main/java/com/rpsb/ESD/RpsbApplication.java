5dda767294b6ea4ab315ff480d4e205734052b03
Anand Pradhan <112967024+Pradhan-80@users.noreply.github.com>
2022-09-30 22:31:37 +0530

    Add files via upload

---
+++ rpsb/src/main/java/com/rpsb/ESD/RpsbApplication.java
@@ -0,0 +1,13 @@
+package com.rpsb.ESD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpsbApplication.class, args);
+	}
+
+}
