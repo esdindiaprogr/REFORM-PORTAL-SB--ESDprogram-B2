3ca183e7a218c5c1bac36e65775de34eb25254c4
esdindiaprogr <114387220+esdindiaprogr@users.noreply.github.com>
2022-11-01 21:13:44 +0530

    Create Angeldatarepo.java

---
+++ rpsb/src/main/java/com/rpsb/ESD/repository/Angeldatarepo.java
@@ -0,0 +1,17 @@
+package com.rpsb.ESD.repo;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.rpsb.ESD.model.AngelData;
+
+
+
+public interface AngelDataRepository extends CrudRepository<AngelData, Long> {
+
+
+
+
+
+}
