23b8eb5c5f131fe97fa2aa58aba17e9d6d85a049
esdindiaprogr <114387220+esdindiaprogr@users.noreply.github.com>
2022-11-21 14:11:27 +0530

    Create signup repo.java

---
+++ rpsb/src/main/java/com/rpsb/ESD/repository/signup repo.java
@@ -0,0 +1,27 @@
+UserRepository
+package com.rpsb.ESD.repository;
+
+import com.springboot.blog.entity.User;
+import org.springframework.data.domain.Example;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsernameOrEmail(String username, String email);
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+}
+RoleRepository
+package com.rpsb.ESD.repository;
+
+import com.springboot.blog.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
