e62edb12c912a5e38a0a68019670ea57c760e6b2
esdindiaprogr <114387220+esdindiaprogr@users.noreply.github.com>
2022-11-21 14:44:50 +0530

    Add files via upload

---
+++ rpsb/src/main/java/com/rpsb/ESD/controller/PostController.java
@@ -0,0 +1,76 @@
+package com.springboot.blog.controller;
+
+import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.payload.PostResponse;
+import com.springboot.blog.service.PostService;
+import com.springboot.blog.utils.AppConstants;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Api(value = "CRUD Rest APIs for Post resources")
+@RestController
+@RequestMapping()
+public class PostController {
+
+    private PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @ApiOperation(value = "Create Post REST API")
+    @PreAuthorize("hasRole('ADMIN')")
+    // create blog post rest api
+    @PostMapping("/api/v1/posts")
+    public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
+        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+
+    @ApiOperation(value = "Get All Posts REST API")
+    // get all posts rest api
+    @GetMapping("/api/v1/posts")
+    public PostResponse getAllPosts(
+            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir
+    ){
+        return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
+    }
+
+    @ApiOperation(value = "Get Post By Id REST API")
+    // get post by id
+    @GetMapping(value = "/api/v1/posts/{id}")
+    public ResponseEntity<PostDto> getPostByIdV1(@PathVariable(name = "id") long id){
+        return ResponseEntity.ok(postService.getPostById(id));
+    }
+
+    @ApiOperation(value = "Update Post By Id REST API")
+    @PreAuthorize("hasRole('ADMIN')")
+    // update post by id rest api
+    @PutMapping("/api/v1/posts/{id}")
+    public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable(name = "id") long id){
+
+       PostDto postResponse = postService.updatePost(postDto, id);
+
+       return new ResponseEntity<>(postResponse, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Delete Post By Id REST API")
+    @PreAuthorize("hasRole('ADMIN')")
+    // delete post rest api
+    @DeleteMapping("/api/v1/posts/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable(name = "id") long id){
+
+        postService.deletePostById(id);
+
+        return new ResponseEntity<>("Post entity deleted successfully.", HttpStatus.OK);
+    }
+}
