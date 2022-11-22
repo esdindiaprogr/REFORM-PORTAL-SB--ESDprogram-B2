f2d92e020937796fdfaa198aee842e6ffacdd9d1
esdindiaprogr <114387220+esdindiaprogr@users.noreply.github.com>
2022-11-22 20:53:10 +0530

    Create signupD.java

---
+++ rpsb/src/main/java/com/rpsb/ESD/signupD.java
@@ -0,0 +1,87 @@
+package com.rpsb.ESD.data;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Entity
+public class SignupTable {
+	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private Long contact_no;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Long getContact_no() {
+		return contact_no;
+	}
+	public void setContact_no(Long contact_no) {
+		this.contact_no = contact_no;
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SignupTable other = (SignupTable) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+	/*
+	 * 
+	 * Getters and Setters
+	 */
+}
+	
