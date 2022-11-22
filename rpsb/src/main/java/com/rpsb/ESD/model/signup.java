eba8fa6c1f3dba405698cd27c239d480fc394e86
esdindiaprogr <114387220+esdindiaprogr@users.noreply.github.com>
2022-11-22 20:55:23 +0530

    Update signup.java

--- rpsb/src/main/java/com/rpsb/ESD/model/signup.java
+++ rpsb/src/main/java/com/rpsb/ESD/model/signup.java
@@ -48,3 +48,68 @@ public class Role {
     @Column(length = 60)
     private String name;
 }
+
+
+
+or
+package com.rpsb.ESD.model;
+
+import java.util.Objects;
+
+public class SignupData {
+	private String firstName;
+	private String lastName;
+	private String email;
+	
+	private Long contact_no;
+	
+	
+	
+	
+//	public String getPassword() {
+//		return password;
+//	}
+//	public void setPassword(String password) {
+//		this.password = password;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Long getContact_no() {
+		return contact_no;
+	}
+	public void setContact_no(Long contact_no) {
+		this.contact_no = contact_no;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(email);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SignupData other = (SignupData) obj;
+		return Objects.equals(email, other.email);
+	}
+	
+}
